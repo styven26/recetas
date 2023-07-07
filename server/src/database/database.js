962ba4a4fea1e800d564c3b96d63f1bbc12db8e8
kevin <kevin.saeteros@espoch.edu.ec>
2023-07-07 13:19:22 -0500

    Imagen nueva

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,17 +2,17 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 export const sequelize = new Sequelize("recetasdb", "postgres", "123", {
-=======
+//=======
 
 //export const sequelize = new Sequelize("recetasdb", "postgres", "2607", {
 
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "awayouname11", {
->>>>>>> 03400e9baef5ff0e640778ef024d5aa78e087b8a
+//export const sequelize = new Sequelize("recetasdb", "postgres", "awayouname11", {
+//>>>>>>> 03400e9baef5ff0e640778ef024d5aa78e087b8a
   host: "localhost",
   logging: false,
   dialect: "postgres",
