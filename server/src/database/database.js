2c31e3c1b3b2f32ef16577a431fdd1083e8a6cb0
kevin <kevin.saeteros@espoch.edu.ec>
2023-07-07 11:56:06 -0500

    ConexionBDS

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,9 +2,10 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
+export const sequelize = new Sequelize("recetasdb", "postgres", "123", {
   host: "localhost",
   dialect: "postgres",
+  port: 5432
 });
 
 
