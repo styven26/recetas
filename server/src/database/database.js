b69ecc786ca3c91ac1ad51af76c270b3721d8fdb
Sebastian <anonblade10@gmail.com>
2023-07-07 11:28:03 -0500

    usuariosController

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,13 +2,14 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-<<<<<<< HEAD
+
+//export const sequelize = new Sequelize("recetasdb", "postgres", "2607", {
+
+
+// conexion a la base de datos
+
 export const sequelize = new Sequelize("recetasdb", "postgres", "awayouname11", {
-=======
-export const sequelize = new Sequelize("recetasdb", "postgres", "2607", {
->>>>>>> 003678c6774825bbb0dbf0466c1f1c8d2144d85f
   host: "localhost",
-  logging: false,
   dialect: "postgres",
   port: 5432
 });
