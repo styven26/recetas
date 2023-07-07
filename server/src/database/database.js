00d4a6f5f72e7dc598763a5cd0a88c4a95b96482
Adrian-Mz <adjrmz23@gmail.com>
2023-07-07 12:56:15 -0500

    Cambios AM

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,7 +2,7 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "2607", {
+export const sequelize = new Sequelize("recetasdb", "postgres", "12345", {
   host: "localhost",
   logging: false,
   dialect: "postgres",
