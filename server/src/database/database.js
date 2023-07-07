7d96aa27faa49f77a9435ef7ee9b42537d0bd9e7
kevin <kevin.saeteros@espoch.edu.ec>
2023-07-07 14:21:51 -0500

    Cambio CSS color

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -3,14 +3,14 @@ import Sequelize from "sequelize";
 // conexion a la base de datos
 
 //<<<<<<< HEAD
-<<<<<<< HEAD
+//<<<<<<< HEAD
 export const sequelize = new Sequelize("recetasdb", "postgres", "123", {
-=======
-export const sequelize = new Sequelize("recetasdb", "postgres", "12345", {
+//=======
+//export const sequelize = new Sequelize("recetasdb", "postgres", "12345", {
 //=======
 //<<<<<<< HEAD
 //export const sequelize = new Sequelize("recetasdb", "postgres", "123", {
->>>>>>> 3d2759131b32ee55563627bff21c70d0c67376c2
+//>>>>>>> 3d2759131b32ee55563627bff21c70d0c67376c2
 //=======
 
 //export const sequelize = new Sequelize("recetasdb", "postgres", "2607", {
@@ -19,10 +19,10 @@ export const sequelize = new Sequelize("recetasdb", "postgres", "12345", {
 // conexion a la base de datos
 
 //export const sequelize = new Sequelize("recetasdb", "postgres", "awayouname11", {
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //>>>>>>> 03400e9baef5ff0e640778ef024d5aa78e087b8a
-=======
->>>>>>> 3d2759131b32ee55563627bff21c70d0c67376c2
+//=======
+//>>>>>>> 3d2759131b32ee55563627bff21c70d0c67376c2
   host: "localhost",
   logging: false,
   dialect: "postgres",
