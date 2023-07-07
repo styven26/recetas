e5ad3354adf940e692638a7692402eadd90dfa27
Adrian-Mz <adjrmz23@gmail.com>
2023-07-07 13:26:59 -0500

    Cambio imagen

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,21 +2,19 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 export const sequelize = new Sequelize("recetasdb", "postgres", "12345", {
-=======
-<<<<<<< HEAD
-export const sequelize = new Sequelize("recetasdb", "postgres", "123", {
-=======
+//=======
+//<<<<<<< HEAD
+//export const sequelize = new Sequelize("recetasdb", "postgres", "123", {
+//=======
 
 //export const sequelize = new Sequelize("recetasdb", "postgres", "2607", {
 
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "awayouname11", {
->>>>>>> 03400e9baef5ff0e640778ef024d5aa78e087b8a
->>>>>>> 2b57ed04d7e234569f07751c55d82eaf2fedbf94
+//export const sequelize = new Sequelize("recetasdb", "postgres", "awayouname11", {
   host: "localhost",
   logging: false,
   dialect: "postgres",
